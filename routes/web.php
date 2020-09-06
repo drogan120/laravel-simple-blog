926fcfb19d9543ba88297b0f11ffd41e1112ba23
drogan120 <alimahmudin.hp@gmail.com>
2020-09-06 21:07:50 +0700

    adding php ui (vue --auth)

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::resource('category', 'CategoryController');
 Route::resource('tag', 'TagController');
 Route::resource('post', 'PostController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
